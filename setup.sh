306cc1fda34ec40046f79956ab9f096b1ef9fcda
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-12-12 13:32:07 +0000

    adds install script for linking files

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+link() {
+  local from=$PWD/$1
+  local to=$2
+
+  [ -L "$2" ] && echo "> link for '$2' already exist ... skpping" && return
+  [ -f "$2" ] && echo "(* ERROR *) '$2' already exist and is file ... aborting" && exit
+  ln -s $from $to
+}
+
+# zshrc
+link "zshrc" "${HOME}/.zshrc"
