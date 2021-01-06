1711c6f04b697a3d3cebaf7b4b46b3bc4b33e172
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-06 09:28:50 +0000

    makes venv extract name based on dir (default)

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,14 +1,14 @@
 venv() {
+  local prompt_name=$1
+
   if [ -z "$1" ]; then
-    echo >&2 "No name passed for the prompt!"
-    echo >&2 "aborting ..."
-    return 1
-  else
-    echo "> creating venv '$1'"
-    python3 -m venv --prompt "$1" .venv
-    echo "> updating pip"
-    .venv/bin/pip install -U pip > /dev/null
+    prompt_name=$(basename $PWD)
   fi
+
+  echo "> creating venv '$prompt_name'"
+  python3 -m venv --prompt "$prompt_name" .venv
+  echo "> updating pip"
+  .venv/bin/pip install -U pip >/dev/null
 }
 
 pulsarvnc() {
