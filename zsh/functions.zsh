b5bd7c3dab6128777b2ea9412afdfee03d4ad68a
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-01-06 11:08:21 +0000

    adds 'activate_virtual_env' function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -11,6 +11,25 @@ create_virtual_env() {
   .venv/bin/pip install -U pip >/dev/null
 }
 
+activate_virtual_env() {
+  local pwd_array=("${(@s:/:)PWD}")
+  local dir="$PWD"
+
+  # go backwards from current directory and find '.venv' directory
+  for ((i=${#pwd_array[@]}; i > 1; i--)); do
+    dir=${(j:/:)pwd_array:0:$i}
+
+    if [ -d "$dir/.venv" ]; then
+      echo "> activating '$dir/.venv'"
+      source $dir/.venv/bin/activate
+      return
+    fi
+  done
+
+  >&2 echo "[ERROR] Did not find a '.venv' directory to active venv"
+  return 1
+}
+
 pulsarvnc() {
   printf "* Port-forwarding of VNC connection to pulsar ... "
   ssh -N -f -L 5900:localhost:5900 pulsar
