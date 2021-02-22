64fb217b63ff901a04b9790605099d4313d6fadd
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-02-22 09:56:41 +0000

    nvim: Turn 'typeCheckingMode' off

--- nvim/lua/ahelm/lsp.lua
+++ nvim/lua/ahelm/lsp.lua
@@ -1,5 +1,14 @@
 --[[ LSP config ]]--
-require'lspconfig'.pyright.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.pyright.setup{
+  on_attach=require'completion'.on_attach,
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "off"
+      }
+    }
+  }
+}
 require'lspconfig'.vimls.setup{}
 require'lspconfig'.bashls.setup{}
 require'lspconfig'.clangd.setup{}
