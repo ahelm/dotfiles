f2b5f1aa334230b603d29295907e6c681581cab8
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-06-27 17:19:40 +0200

    nvim: change bufferline style to thin

--- nvim/lua/ahelm/bufferline.lua
+++ nvim/lua/ahelm/bufferline.lua
@@ -1,7 +1,7 @@
 vim.g.termguicolors = true
 require("bufferline").setup {
   options = {
-    separator_style = "thick",
+    separator_style = "thin",
     always_show_bufferline = false,
     show_close_icon = false,
     show_buffer_close_icons = false,
