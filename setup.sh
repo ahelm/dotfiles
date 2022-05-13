f2bb0e20cfb79ee02e54a580a59665248788f2df
Anton Helm <a.helm87@gmail.com>
2022-05-13 18:43:53 +0200

    Change to brew for ubunut/linux installation

--- setup.sh
+++ setup.sh
@@ -10,19 +10,13 @@ warn() {
 log() {
   echo "> $1"
 }
-ensure_installed_brew() {
+ensure_installed() {
+  if ! command -v brew &> /dev/null; then
+    err "Requires brew to be installed"
+    exit 1
+  fi
   log "checking installation for '$1'"
   brew list $1 &>/dev/null || brew install $1
-}
-
-ensure_installed_cargo() {
-  log "checking installation for '$1'"
-  cargo install $1
-}
-
-ensure_installed_apt() {
-  log "checking installation for '$1'"
-  sudo apt install $1
 }
 
 link() {
@@ -59,45 +53,23 @@ link zshrc	${HOME}/.zshrc
 link gitconfig	${HOME}/.gitconfig
 link gitignore	${HOME}/.gitignore
 link vimrc	${HOME}/.vimrc
-# link nvim	${HOME}/.config/nvim
+link nvim	${HOME}/.config/nvim
 
 # handy packages
 # installed by brew, apt, snap or other package managers
-if [ "$(uname)" == "Darwin" ]; then
-  ensure_installed_brew bat
-  ensure_installed_brew exa
-  ensure_installed_brew dust
-  ensure_installed_brew fd
-  ensure_installed_brew ripgrep
-  ensure_installed_brew git
+ensure_installed bat
+ensure_installed exa
+ensure_installed dust
+ensure_installed fd
+ensure_installed ripgrep
+ensure_installed git
+ensure_installed neovim
 
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  # UBUNTU
-  if [ -n "$(uname -a | grep Ubuntu)" ]; then
-    # make sure rust is installed
-    if ! command -v cargo &> /dev/null; then
-      sudo apt update && sudo apt upgrade && sudo apt install -y curl gcc
-      warn "Missing a valid rust installation"
-      curl https://sh.rustup.rs -sSf | sh
-      source $HOME/.cargo/env
-    fi
-    
-    ensure_installed_cargo bat
-    ensure_installed_cargo exa
-    ensure_installed_cargo du-dust
-    ensure_installed_cargo ripgrep
-
-    # Requires 'make' to be installed for 'fd-find'
-    ensure_installed_apt make
-    cargo install fd-find
-
-    # zsh highlighting
-    ensure_installed_apt zsh-syntax-highlighting
-
-    ensure_installed_apt git
-  else
-    echo "Invalid system for tool installations"
-  fi
+# install packer for neovim
+if [ ! -d $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
+  log "cloning 'packer'"
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim  \
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
 
 # fzf installation
