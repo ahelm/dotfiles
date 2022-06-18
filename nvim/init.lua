35c20e9b05577100a11b9fb9414649273bbf1ae7
Anton Helm <a.helm87@gmail.com>
2022-06-18 02:32:26 +0200

    Add general nvim settings and plugins module

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+require "user.general"
+
 -- require('ahelm.plugins')
 -- require('ahelm.general')
 -- require('ahelm.keymaps')
@@ -8,3 +10,4 @@
 -- require('ahelm.nvimtree')
 require("user.keymaps")
 require("user.autocommands")
+require("user.plugins")
