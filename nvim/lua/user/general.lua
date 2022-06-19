a48ae95df097ccf2771264814dbcdaccbccfb000
Anton Helm <a.helm87@gmail.com>
2022-06-19 21:27:49 +0200

    Update general settings to previous settings

--- nvim/lua/user/general.lua
+++ nvim/lua/user/general.lua
@@ -1,1 +1,14 @@
 vim.opt.termguicolors = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.wrap = false
+
+vim.opt.number = true
+-- vim.opt.signcolumn = "yes"
+
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
