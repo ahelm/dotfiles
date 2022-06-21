1cfa59c256d0ee83b11a60257d847f4ded62f042
Anton Helm <a.helm87@gmail.com>
2022-06-21 16:04:26 +0200

    nvim: Add toggleterm as plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -52,6 +52,7 @@ return packer.startup(function(use)
   use "kyazdani42/nvim-tree.lua"
   use "akinsho/bufferline.nvim"
   use "nvim-lualine/lualine.nvim"
+  use "akinsho/toggleterm.nvim"
 
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
