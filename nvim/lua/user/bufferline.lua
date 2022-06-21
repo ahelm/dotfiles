f9211b4bdc367d1372c96ddb18cc84356aa48aac
Anton Helm <a.helm87@gmail.com>
2022-06-21 15:41:57 +0200

    nvim: Change to buffer switching using bufferline

--- nvim/lua/user/bufferline.lua
+++ nvim/lua/user/bufferline.lua
@@ -11,3 +11,10 @@ bufferline.setup {
     show_buffer_close_icons = false,
   }
 }
+
+-- Buffer switching
+local keymap = vim.api.nvim_set_keymap
+keymap('n', '<leader>n', '<cmd>BufferLineCyclePrev<cr>', { noremap = true })
+keymap('n', '<leader>m', '<cmd>BufferLineCycleNext<cr>', { noremap = true })
+keymap('i', '<leader>n', '<cmd>BufferLineCyclePrev<cr>', { noremap = true })
+keymap('i', '<leader>m', '<cmd>BufferLineCycleNext<cr>', { noremap = true })
--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -21,12 +21,6 @@ keymap("n", "<c-j>", "<c-w><c-j>", opts)
 keymap("n", "<c-k>", "<c-w><c-k>", opts)
 keymap("n", "<c-l>", "<c-w><c-l>", opts)
 
--- Buffer switching
-keymap("n", "<leader>n", "<cmd>bprevious<cr>", opts)
-keymap("n", "<leader>m", "<cmd>bnext<cr>", opts)
-keymap("i", "<leader>n", "<cmd>bprevious<cr>", opts)
-keymap("i", "<leader>m", "<cmd>bnext<cr>", opts)
-
 -- Search
 keymap("n", "<leader> ", ":nohlsearch<cr>", opts)
 
